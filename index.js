7075a5e55a863b71eeb69c9a6871c3ada3617810
Dalton Carlile <dlcarlile93@hotmail.com>
2022-09-29 16:04:58 -0700

    commit tree a952d7266278868f582a972108640d0109d6c64c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+
+}
+
+// function wrapAdjective(adj = "special"){
+//     const encouragingPromptFunction = wrapAdjective("!!!")
+//     wrapAdjective("%")(`${adj}`)
+//     return `You are ${adj}`
+// }   
+
+function wrapAdjective(style = "*" ) {
+        return function (adj = "special"){
+            return (`You are ${style}${adj}${style}!`)
+        }
+}
+    // hello()
+console.log(wrapAdjective)
+// console.log(wrapAdjective("%"))
+console.log(wrapAdjective()())
+// console.log(wrapAdjective("%")("any")())
+
+// let result = wrapAdjective("||")
+// let emphatic = result("a dedicated programmer")
+// console.log("result",result)
+// console.log("emphatic",emphatic)
+// console.log("expect",emphatic,"to equal,You are ||a dedicated programmer||! ")
+
+
+
+
+
+
+
+ 
